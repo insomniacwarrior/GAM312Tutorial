cedb2c4bdefdeb9278076f2ba7dcec65afbe225a
Nicolas Solis <nicolas.solis@snhu.edu>
2026-09-02 09:52:16 -0500

    Initialized movement and camera via C++, ready to start!

--- Source/GAM312Tutorial/PlayerChar.cpp
+++ Source/GAM312Tutorial/PlayerChar.cpp
@@ -8,6 +8,12 @@ APlayerChar::APlayerChar()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	PlayerCamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("First Person Cam"));
+
+	PlayerCamComp->SetupAttachment(GetMesh(), "head");
+
+	PlayerCamComp->bUsePawnControlRotation = true;
+
 
 }
 
@@ -29,6 +35,36 @@ void APlayerChar::Tick(float DeltaTime)
 void APlayerChar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerChar::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerChar::MoveRight);
+	PlayerInputComponent->BindAxis("LookUp", this, &APlayerChar::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("Turn", this, &APlayerChar::AddControllerYawInput);
+	PlayerInputComponent->BindAction("JumpEvent", IE_Pressed, this, &APlayerChar::StartJump);
+	PlayerInputComponent->BindAction("JumpEvent", IE_Released, this, &APlayerChar::StopJump);
 }
 
+void APlayerChar::MoveForward(float axisValue)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, axisValue);
+}
+
+void APlayerChar::MoveRight(float axisValue)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Direction, axisValue);
+}
+
+void APlayerChar::StartJump()
+{
+	bPressedJump = true;
+}
+
+void APlayerChar::StopJump()
+{
+	bPressedJump = false;
+}
+
+void APlayerChar::FindObject()
+{
+}
--- Source/GAM312Tutorial/PlayerChar.h
+++ Source/GAM312Tutorial/PlayerChar.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
 #include "PlayerChar.generated.h"
 
 UCLASS()
@@ -25,5 +26,24 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	UFUNCTION()
+		void MoveForward(float axisValue);
+
+	UFUNCTION()
+		void MoveRight(float axisValue);
+
+	UFUNCTION()
+		void StartJump();
+
+	UFUNCTION()
+		void StopJump();
+
+	UFUNCTION()
+		void FindObject();
+
+	UPROPERTY(VisibleAnywhere)
+		UCameraComponent* PlayerCamComp;
 
 };
