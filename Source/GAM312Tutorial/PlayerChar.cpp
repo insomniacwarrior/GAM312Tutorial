64e815ee75cfa6447b85168c95ff401e2a3984bc
Nicolas Solis <nicolas.solis@snhu.edu>
2026-09-07 11:16:34 -0500

    Established the base for the Player's Stat System

--- Source/GAM312Tutorial/PlayerChar.cpp
+++ Source/GAM312Tutorial/PlayerChar.cpp
@@ -22,6 +22,11 @@ APlayerChar::APlayerChar()
 void APlayerChar::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// Sets a timer to call the DecreaseStats function every 2 seconds
+	FTimerHandle StatsTimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(StatsTimerHandle, this, &APlayerChar::DecreaseStats, 2.0f, true);
+
 	
 }
 
@@ -73,4 +78,55 @@ void APlayerChar::StopJump()
 
 void APlayerChar::FindObject()
 {
+}
+
+///////////////////////////
+// These functions determine the mechanic for ADDING TO the player's stats.
+///////////////////////////
+
+void APlayerChar::SetHealth(float amount)
+{
+	if (Health + amount < 100)
+	{
+		Health = Health + amount;
+
+	}
+}
+
+void APlayerChar::SetHunger(float amount)
+{
+	if (Hunger + amount < 100)
+	{
+		Hunger = Hunger + amount;
+
+	}
+}
+
+void APlayerChar::SetStamina(float amount)
+{
+	if (Stamina + amount < 100)
+	{
+		Stamina = Stamina + amount;
+
+	}
+}
+
+///////////////////////////
+// This function will essentially trade hunger to regenerate stamina on each iteration.
+// If the player has no more hunger, this instead eats into their remaining health.
+///////////////////////////
+
+void APlayerChar::DecreaseStats()
+{
+	if (Hunger > 0)
+	{
+		SetHunger(-1.0f);
+	}
+	
+	SetStamina(10.0f);
+
+	if (Hunger <= 0)
+	{
+		SetHealth(-3.0f);
+	}
 }
--- Source/GAM312Tutorial/PlayerChar.h
+++ Source/GAM312Tutorial/PlayerChar.h
@@ -27,7 +27,10 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	// Functions to handle player movement and actions; further elaborated in the c++ file.
+	///////////////////////////
+	// Functions(Definitions) to handle player movement and actions; further elaborated in the c++ file.
+	///////////////////////////
+
 	UFUNCTION()
 		void MoveForward(float axisValue);
 
@@ -45,5 +48,39 @@ public:
 
 	UPROPERTY(VisibleAnywhere)
 		UCameraComponent* PlayerCamComp;
+	
+	///////////////////////////
+	// Properties to hold the player's stats. 
+	// 
+	// UPROPERTY establishes a variable.
+	// EditAnywhere allows the variable to be edited(grants visibility) within the Class.
+	// BlueprintReadWrite allows the variable to be read and written to within the Blueprint(GUI).
+	// The Category is used to group the variable(s) within the Class(Details Panel Dropdown!).
+	///////////////////////////
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+		float Health = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+		float Stamina = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Stats")
+		float Hunger = 100.0f;
+
+	///////////////////////////
+	// Functions that define the behavior of the player's stats. Defined within the C++ file.
+	///////////////////////////
+
+	UFUNCTION(BlueprintCallable)
+		void SetHealth(float amount);
+	
+	UFUNCTION(BlueprintCallable)
+		void SetHunger(float amount);
+
+	UFUNCTION(BlueprintCallable)
+		void SetStamina(float amount);
+		
+	UFUNCTION()
+		void DecreaseStats();
 
 };
