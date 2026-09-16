a44f9ee275c1b97d3514a0f043cdb1e079b74d25
Nicolas Solis <nicolas.solis@snhu.edu>
2026-09-16 11:04:59 -0500

    Added a building mode and UI to the tutorial project

--- Source/GAM312Tutorial/PlayerChar.cpp
+++ Source/GAM312Tutorial/PlayerChar.cpp
@@ -25,6 +25,8 @@ APlayerChar::APlayerChar()
 	ResourcesNameArray.Add(TEXT("Wood"));
 	ResourcesNameArray.Add(TEXT("Stone"));
 	ResourcesNameArray.Add(TEXT("Berry"));
+	// Setup the Building Array; this should match the Resources Array count.
+	BuildingArray.SetNum(3);
 
 
 }
@@ -46,6 +48,17 @@ void APlayerChar::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (isBuilding)
+	{
+		if (spawnedPart) //This provides the player with the location they can place an object until they click again.
+		{
+			FVector StartLocation = PlayerCamComp->GetComponentLocation();
+			FVector Direction = PlayerCamComp->GetForwardVector() * 400.0f;
+			FVector EndLocation = StartLocation + Direction;
+			spawnedPart->SetActorLocation(EndLocation);
+		}
+	}
+	
 }
 
 // Called to bind functionality to input
@@ -60,6 +73,7 @@ void APlayerChar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAction("JumpEvent", IE_Pressed, this, &APlayerChar::StartJump);
 	PlayerInputComponent->BindAction("JumpEvent", IE_Released, this, &APlayerChar::StopJump);
 	PlayerInputComponent->BindAction("Interact", IE_Pressed, this, &APlayerChar::FindObject);
+	PlayerInputComponent->BindAction("RotPart", IE_Pressed, this, &APlayerChar::RotateBuilding);
 }
 
 void APlayerChar::MoveForward(float axisValue)
@@ -110,6 +124,7 @@ void APlayerChar::GiveResource(float amount, FString resourceType)
 	}
 }
 
+
 ///////////////////////////
 // This function serves as a RayCast, enabling Player Interactions via a left click.
 ///////////////////////////
@@ -124,54 +139,63 @@ void APlayerChar::FindObject()
 	QueryParams.AddIgnoredActor(this); //Ignores self when casting
 	QueryParams.bTraceComplex = true; //Traces Complex Objects(Collision)
 	QueryParams.bReturnFaceIndex = true; //Returns the index of the face of the object hit
-
-	// This bit here is where the cast is performed, and returns the relevant information to the HitResult.
-	if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, QueryParams))
+	
+	if (!isBuilding) //Checks if the player is not building before the actual cast.
 	{
-		AResource_M* HitResource = Cast<AResource_M>(HitResult.GetActor()); //Sets variable to HitResource if ray hits an actual resource
-
-		if (Stamina > 5.0f) //Checks if stamina is there to consume first
+		// This bit here is where the cast is performed, and returns the relevant information to the HitResult.
+		if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, QueryParams))
 		{
-			if (HitResource)
+			AResource_M* HitResource = Cast<AResource_M>(HitResult.GetActor()); //Sets variable to HitResource if ray hits an actual resource
+
+			if (Stamina > 5.0f) //Checks if stamina is there to consume first
 			{
-				FString hitName = HitResource->resourceName; //Gets the name of the resource hit
-				int resourceValue = HitResource->resourceAmount; //Gets the amount of the resource obtained
-
-				HitResource->totalResource = HitResource->totalResource - resourceValue; //Decreases the total "health" of the hit resource.
-
-				// If the resource has "health" remaining, collect it. Otherwise, Destroy it.
-				if (HitResource->totalResource > resourceValue)
+				if (HitResource)
 				{
-					GiveResource(resourceValue, hitName);
+					FString hitName = HitResource->resourceName; //Gets the name of the resource hit
+					int resourceValue = HitResource->resourceAmount; //Gets the amount of the resource obtained
 
-					check(GEngine != nullptr); //Ensures the engine's subsystem can display the text before firing.
-					//This will be the variable passed into the string later for totals.
-					int32 PlayerTotal = 0;
-					//Go down the list, seeking out the hit resource.
-					if (hitName == "Wood")   PlayerTotal = ResourcesArray[0];
-					else if (hitName == "Stone")  PlayerTotal = ResourcesArray[1];
-					else if (hitName == "Berry")  PlayerTotal = ResourcesArray[2];
-					//Formats the intended string before pushing it out to the debug message.
-					FString ResMessage = FString::Printf(TEXT("Total %s collected: %d"), *hitName, PlayerTotal);
-					GEngine->AddOnScreenDebugMessage(-1, 1.5f, FColor::Green, ResMessage);
-					// Decal goes splat
-					UGameplayStatics::SpawnDecalAtLocation(GetWorld(), hitDecal, FVector(10.0f,10.0f,10.0f), HitResult.Location, FRotator(-90, 0,0), 2.0f);
-					
-					SetStamina(-5.0f); //Consumes the stamina
-				}
-				else
-				{
-					HitResource->Destroy();
-					check(GEngine != nullptr);
-					GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Resource Depleted!"));	
+					HitResource->totalResource = HitResource->totalResource - resourceValue; //Decreases the total "health" of the hit resource.
+
+					// If the resource has "health" remaining, collect it. Otherwise, Destroy it.
+					if (HitResource->totalResource > resourceValue)
+					{
+						GiveResource(resourceValue, hitName);
+
+						check(GEngine != nullptr); //Ensures the engine's subsystem can display the text before firing.
+						//This will be the variable passed into the string later for totals.
+						int32 PlayerTotal = 0;
+						//Go down the list, seeking out the hit resource.
+						if (hitName == "Wood")   PlayerTotal = ResourcesArray[0];
+						else if (hitName == "Stone")  PlayerTotal = ResourcesArray[1];
+						else if (hitName == "Berry")  PlayerTotal = ResourcesArray[2];
+						//Formats the intended string before pushing it out to the debug message.
+						FString ResMessage = FString::Printf(TEXT("Total %s collected: %d"), *hitName, PlayerTotal);
+						GEngine->AddOnScreenDebugMessage(-1, 1.5f, FColor::Green, ResMessage);
+						// Decal goes splat
+						UGameplayStatics::SpawnDecalAtLocation(GetWorld(), hitDecal, FVector(10.0f,10.0f,10.0f), HitResult.Location, FRotator(-90, 0,0), 2.0f);
+						
+						SetStamina(-5.0f); //Consumes the stamina
+					}
+					else
+					{
+						HitResource->Destroy();
+						check(GEngine != nullptr);
+						GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Resource Depleted!"));	
+					}
 				}
 			}
+			else
+			{
+				check(GEngine != nullptr);
+				GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, TEXT("You are too tired to gather the resource at the moment."));
+			}
 		}
-		else
-		{
-			check(GEngine != nullptr);
-			GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, TEXT("You are too tired to gather the resource at the moment."));
-		}
+	}
+	
+	else
+	{
+		isBuilding = false;
+		
 	}
 	
 }
@@ -185,7 +209,10 @@ void APlayerChar::SetHealth(float amount)
 	if (Health + amount < 100)
 	{
 		Health = Health + amount;
-
+	}
+	else if (Health + amount >= 100)
+	{
+		Health = 100;
 	}
 }
 
@@ -196,6 +223,10 @@ void APlayerChar::SetHunger(float amount)
 		Hunger = Hunger + amount;
 
 	}
+	else if (Hunger + amount >= 100)
+	{
+		Hunger = 100;
+	}
 }
 
 void APlayerChar::SetStamina(float amount)
@@ -204,6 +235,10 @@ void APlayerChar::SetStamina(float amount)
 	{
 		Stamina = Stamina + amount;
 
+	}
+	else if (Stamina + amount >= 100)
+	{
+		Stamina = 100;
 	}
 }
 
@@ -224,5 +259,73 @@ void APlayerChar::DecreaseStats()
 	if (Hunger <= 0)
 	{
 		SetHealth(-3.0f);
+	}
+}
+
+///////////////////////////
+// These functions will handle the updating of resources and basic building mechanics.
+///////////////////////////
+
+void APlayerChar::UpdateResources(float woodAmount, float stoneAmount, FString buildingObject)
+{
+	// Updates our Resource Count and adds the building to its respective array.
+	if (woodAmount <= ResourcesArray[0])
+	{
+		if (stoneAmount <= ResourcesArray[1])
+		{
+			ResourcesArray[0] = ResourcesArray[0] - woodAmount;
+			ResourcesArray[1] = ResourcesArray[1] - stoneAmount;
+			
+			if (buildingObject == "Wall")
+			{
+				BuildingArray[0] = BuildingArray[0] + 1;
+			}
+			
+			if (buildingObject == "Floor")
+			{
+				BuildingArray[1] = BuildingArray[1] + 1;
+			}
+			
+			if (buildingObject == "Ceiling")
+			{
+				BuildingArray[2] = BuildingArray[2] + 1;
+			}
+			
+		}
+	}
+}
+
+void APlayerChar::SpawnBuilding(int buildingID, bool& isSuccess)
+{
+	if (!isBuilding)
+	{
+		if (BuildingArray[buildingID] >= 1) //Checks to see if the player has any of the object passed through
+		{
+			isBuilding = true;
+			FActorSpawnParameters SpawnParams;
+			FVector StartLocation = PlayerCamComp->GetComponentLocation();
+			FVector Direction = PlayerCamComp->GetForwardVector() * 400.0f;
+			FVector EndLocation = StartLocation + Direction;
+			FRotator myRot(0, 0, 0);
+			
+			BuildingArray[buildingID] = BuildingArray[buildingID] - 1;
+			
+			spawnedPart = GetWorld()->SpawnActor<ABuildingPart>(BuildPartClass, EndLocation, myRot, SpawnParams);
+			
+			isSuccess = true;
+		}
+	}
+	else
+		isSuccess = false;
+	{
+		
+	}
+}
+
+void APlayerChar::RotateBuilding()
+{
+	if (isBuilding)
+	{
+		spawnedPart->AddActorWorldRotation(FRotator(0, 90, 0));
 	}
 }
